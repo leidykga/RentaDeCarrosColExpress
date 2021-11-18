e329ca5814fbf5b2c49f8867e8d860fa5ccce032
Daniel077012 <daniel077@live.com>
2021-11-18 15:19:46 -0500

    Daniel

--- src/controllers/cliente.controller.ts
+++ src/controllers/cliente.controller.ts
@@ -4,18 +4,12 @@ import {
   Filter,
   FilterExcludingWhere,
   repository,
-  Where,
+  Where
 } from '@loopback/repository';
 import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
+  del, get,
+  getModelSchemaRef, param, patch, post, put, requestBody,
+  response
 } from '@loopback/rest';
 import {Cliente} from '../models';
 import {ClienteRepository} from '../repositories';
@@ -23,8 +17,8 @@ import {ClienteRepository} from '../repositories';
 export class ClienteController {
   constructor(
     @repository(ClienteRepository)
-    public clienteRepository : ClienteRepository,
-  ) {}
+    public clienteRepository: ClienteRepository,
+  ) { }
 
   @post('/clientes')
   @response(200, {
@@ -44,7 +38,7 @@ export class ClienteController {
     })
     cliente: Omit<Cliente, 'id'>,
   ): Promise<Cliente> {
-    return this.clienteRepository.create(cliente);
+    let p = await this.clienteRepository.create(cliente);
   }
 
   @get('/clientes/count')
